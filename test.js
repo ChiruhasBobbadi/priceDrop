321d04f04011dde47bcf816931c3f2e36c825e0f
Chiruahs <chiruhas.bobbadi123@gmail.com>
2019-11-02 00:24:33 +0530

    scraping list page is almost done.

--- test.js
+++ test.js
@@ -2,28 +2,43 @@ const cheerio = require('cheerio');
 
 const request = require('request');
 
+const product='galaxy buds';
+const page='1';
+const url=`https://www.amazon.in/s?k=${product}&page=${page}`;
+
+request.get(url, function (
+    error,
+    response,
+    data
+) {
+
+    const $ = cheerio.load(data);
+    let len = 0;
+    let products = [];
+    $(".s-result-list  .celwidget ").each((i, el) => {
+
+        //console.log($(el).attr("src"));
+        len++;
+        // image url
+        const imageUrl = $(el).find('.s-image').attr('src');
 
 
-    request.get(`https://www.amazon.in/s?k=lightning+deals+of+the+day&crid=2Z56CTOY0M7SQ&sprefix=ligh%2Caps%2C300&ref=nb_sb_ss_i_1_4`, function(
-        error,
-        response,
-        data
-    ) {
-        const $ = cheerio.load(data);
-        let len=0;
-        const text = $(".s-result-list  .celwidget ").each((i,elm)=>{
+        //current price
+        const temp = $(el).find('.a-price .a-price-whole');
+        const curr_price = (String(temp).split("</span>")[0]).split('>')[1];
 
-                len++;
-            });
-        console.log(len);
+        // actual price
+        const ac_price = $(el).find('.a-price .a-offscreen').text();
 
-        ;
+        const p_name = $(el).find('.s-image').attr('alt');
 
-        // if(!text){
-        //     console.log("NO Deal ");
-        // }
-        // else{
-           // console.log(text);
-        //}
+        products.push({"p_name": p_name, "p_actual_price": ac_price, "p_current_price": curr_price, "image_url": imageUrl});
+
+    len++;
+
+    });
+
+    console.log(products);
+    console.log(len);
 
 });
